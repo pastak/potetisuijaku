7e9c4d49df1e575c21de86dd3b47faee0a7f254e
pastak <pasta0915@gmail.com>
2013-08-24 19:52:35 +0900

    一応完成しました

--- main.js
+++ main.js
@@ -1,5 +1,45 @@
-$(function(){
-  for(var i = 0;i<40;i++){
-    $('#suijaku').append($('<img class="poteti-card" src="./poteti.jpeg">'));
+$(
+function(){
+  var pre = '';
+  function Message(mes,url) {
+    $('#message').show().text(mes).append($('<a href="'+url+">").text('twitterで見る'));
   }
+  $('#blank').css({
+    'height': $(document).height(),
+    'width': $(document).width()
+  }).on('click',function(){
+      $('#blank').hide();
+      $('#message').hide();
+  });
+  $('.poteti-card').on('click',function(){
+    if($(this).is('.pre')){return false}
+    $('#blank').show();
+    Message($(this).attr('data-text'),$(this).attr('data-url'));
+    if(pre == ''){
+      pre = $(this).attr('data-url');
+      $(this).addClass('pre').removeClass('poteti-card');
+    }else{
+      if(pre == $(this).attr('data-url')){
+        alert('正解！！');
+        $(this).addClass('ok');
+        $('.pre').addClass('ok').removeClass('pre').removeClass('poteti-card');
+      }
+      pre = '';
+      $('.pre').removeClass('pre').addClass('poteti-card');
+    }
+  });
+  $.getJSON('./poteti.json').done(function(data){
+    data.sort(function() {
+            return Math.random() - Math.random();
+    });
+    var cards = $('.poteti-card');
+    $.each(data,function(i,item){
+      cards[i].id = i;
+      localStorage[('card'+i)] = {
+        text:'',
+        url:''
+      };
+      $(cards[i]).attr('data-text',item.text).attr('data-url',item.url);
+    });
+  });
 });
